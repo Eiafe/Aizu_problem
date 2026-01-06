b7e2e655f8af30b0d3fb5633e75d8087bae80df9
Md Mahathir Ullah Yafi <mahathirullahyafikhan@gmail.com>
2026-01-06 12:18:15 +0600

    Add files via upload
    find reverce number

---
+++ main (3).cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int t;
+   cin>>t;
+   int sum=0;
+   while(t>0){
+       int last=t%10;
+       sum*=10;
+       sum+=last;
+      t/=10; 
+       
+   }
+   cout<<sum<<endl;
+   
+}
