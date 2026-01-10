d3af3edd0812492b322e43a6bfaf1122c80f4896
Md Mahathir Ullah Yafi <mahathirullahyafikhan@gmail.com>
2026-01-10 10:05:53 +0600

    Add files via upload
    printfing 1 to 5 shape

--- main (1).cpp
+++ main (1).cpp
@@ -1,15 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int t;
-    int mini=1000000;
-    cin>>t;
-    for(int i=1;i<=t;i++){
-        int x;
-        cin>>x;
-        if(x<mini){
-            mini=x;
+int main (){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
         }
+        cout<<"\n";
+        
     }
-     cout<<mini<<endl;
+    
 }
