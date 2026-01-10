6d03156aef9bc11dbfd0b80d57ebfdfeace9f334
Md Mahathir Ullah Yafi <mahathirullahyafikhan@gmail.com>
2026-01-10 10:05:22 +0600

    Add files via upload
    Printing loop shape

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int t;
-    int max=-2000000;
-    cin>>t;
-    for(int i=1;i<=t;i++){
-        int x;
-        cin>>x;
-        if(x>max){
-            max=x;
+int main (){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"1";
         }
-        cout<<max;
+        cout<<"\n";
+        
     }
 }
