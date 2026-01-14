e69444ed04c8afcd6db4b56149b6e22fc9d66411
Md Mahathir Ullah Yafi <mahathirullahyafikhan@gmail.com>
2026-01-14 19:49:43 +0600

    Add files via upload

---
+++ hypotenuse.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=i+1;j<=n;j++){
+
+            for(int k=j+1;k<=n;k++){
+                if(k*k== j*j+i*i){
+                        cout<<i<<" "<<j<<" "<<k<<" "<<endl;
+
+                }
+
+            }
+        }
+    }
+
+}
+
