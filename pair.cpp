3c3fad408bf6852d106dfaab3a636da226d85553
Md Mahathir Ullah Yafi <mahathirullahyafikhan@gmail.com>
2026-01-14 08:34:22 +0600

    Add files via upload

---
+++ pair.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=i+1;j<=n;j++){
+            cout<<i;
+            cout<<j<<endl;
+        }
+
+    }
+
+}
